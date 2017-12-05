71506793f764ceee42c9c45ee8761db69c023b32
Francisco Trindade <francisco.ad.trindade@gmail.com>
2017-12-05 09:45:58 -0500

    Day 5 solutions

---
+++ day5/part1/part1.py
@@ -0,0 +1,20 @@
+def main():
+    with open('part1.in') as f:
+        content = f.readlines()
+    content = [int(x.strip()) for x in content]
+
+    num_steps = traverse_list(content)
+    print(num_steps)
+
+def traverse_list(maze):
+    index = 0
+    num_steps = 0
+    while (index > -1 and index < len(maze)):
+        new_index = index + maze[index]
+        maze[index] += 1
+        num_steps += 1
+        index = new_index
+    return num_steps
+
+if __name__ == '__main__':
+    main()
